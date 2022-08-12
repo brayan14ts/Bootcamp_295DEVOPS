c3c0a9d41096bae4335f12bb7fcd094455021335
Brayan Torrealba <brayan14ts@gmail.com>
2022-08-12 04:06:10 -0300

    DOCS:Actualización Reto 01. Se añadio idea inicial de resolución al ejercicio 4. Issue:2

---
+++ Challenge-2-Linux/01/exercise4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+#Crea un script de bash que descargue el contenido de una página web a un fichero.
+echo "Descargando archivo"
+wget "https://es.wikipedia.org/wiki/DevOps"
+
+# en el mismo una palabra dada (esta se pasará por parametro) y muestre por pantalla el número de linea donde aparece.
+read $palabra
+
+grep
