1f3a0f9984be530e4d639213ff704e3cc900cbf6
Brayan Torrealba <brayan14ts@gmail.com>
2022-08-12 22:34:49 -0300

    DOCS:Actualización Reto 01. Ejercicio 4 completado. Issue:4

--- Challenge-2-Linux/01/exercise4.sh
+++ Challenge-2-Linux/01/exercise4.sh
@@ -1,11 +1,23 @@
 #!/bin/bash 
 
-
+echo "inicializando fichero:" $0"..."
 #Crea un script de bash que descargue el contenido de una página web a un fichero.
-echo "Descargando archivo"
-wget "https://es.wikipedia.org/wiki/DevOps"
+echo "Descargando index.html desde https://www.linkedin.com/"
+wget "https://www.linkedin.com/"
 
 # en el mismo una palabra dada (esta se pasará por parametro) y muestre por pantalla el número de linea donde aparece.
-read $palabra
 
-grep
+if [ -z "$1" ]
+then
+  echo "Error: No ingresaste ninguna parametro al ejecutar el script."
+  echo "Ingresa una palabra ahora"
+  read palabra
+  echo "La palabra" $palabra "coincide con las lineas:"
+  grep -n --color=always $palabra index.html
+
+else
+  echo "Buscando desde index.html, la palabra:" $1"."
+  echo "La palabra" $1 "coincide con las lineas:"
+  grep -n --color=always $1 index.html
+fi
+echo "Fin."
